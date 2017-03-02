cc2f33f45e5c550174fe225c452b0af36128220a
TOSHIKI-PC <wwjd.baske.0107@gmail.com>
2017-03-03 05:04:44 +0900

    install express/bootstrap/bower

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
-const http = require('http');
-const server = http.createServer();
-const fs = require('fs');
-const socketio = require('socket.io');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-server.on('request', function (req, res) {
-  fs.readFile('./client/index.html', 'utf8', function (err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-type': 'text/plain'});
-      res.write('page not found!!');
-      return res.end();
-    }
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/public', express.static('public'));
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-const port = 3000;
-server.listen(port, function () {
-  console.log('server runing on port ' + port);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-const io = socketio.listen(server);
-io.sockets.on('connection', function (socket) {
-  // console.log(socket);
-  socket.on('message', function (data) {
-    // console.log(data.value);
-    io.sockets.emit('from_server', {value: data.value});
-  });
-});
-
-// socket.broadcast.emit('in', {value: "login "}); //自分以外に送信
+module.exports = app;
---
+++ public/javascripts/chat.js
@@ -0,0 +1,23 @@
+var socket = io.connect();
+
+function send(elm) {
+  var forms = elm.form;
+  var name = forms.name;
+  var message = forms.message;
+  if (name.value != '' && message.value != '') {
+    socket.emit('message', {value: {'name': name.value, 'message': message.value}});
+  } else {
+    var dashboard = document.getElementByid('messageForm');
+    dashboard[0].insertAdjacentHTML('afterend', "<p style='color: red;'>※全部入力してね</p>");
+  }
+  message.value = '';
+}
+
+socket.on('from_server', function (data) {
+  write(data.value);
+});
+
+function write(message) {
+  var dashboard = document.getElementsByClassName('dashboard');
+  dashboard[0].insertAdjacentHTML('beforeend', "<p>" + message.name + ": " + message.message + "</p>");
+}
